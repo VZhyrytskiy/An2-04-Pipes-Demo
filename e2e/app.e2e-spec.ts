fa3e49481bdd6ac4d949d0f908b702e3f5d32848
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-11-13 14:54:52 +0200

    updated angular to v5

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { NewPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('new App', function() {
-  let page: NewPage;
+describe('an App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new NewPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
