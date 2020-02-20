edab45e56e6aadb00db23a135ee7f5ff378091ea
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2020-02-20 16:42:52 +0200

    refactor structure of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { BuiltInPipesModule } from './01-built-in-pipes/built-in-pipes.module';
-import { UsePipeInCodeModule } from './03-use-pipe-in-code/use-pipe-in-code.module';
-import { UsePurePipeModule } from './04-use-pure-pipe/use-pure-pipe.module';
-import { UsePipeResultModule } from './05-use-pipe-result/use-pipe-result.module';
+import { UsePipeInCodeModule } from './02-use-pipe-in-code/use-pipe-in-code.module';
+import { UsePurePipeModule } from './03-use-pure-pipe/use-pure-pipe.module';
+import { UsePipeResultModule } from './04-use-pipe-result/use-pipe-result.module';
 
 @NgModule({
   declarations: [AppComponent],
