93c679add40076456708ceb3fcfb5bda1e8cb871
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2020-02-20 16:29:17 +0200

    partly migrate project to v9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { BuiltInPipesModule } from './01-built-in-pipes/built-in-pipes.module';
-import { DepricatedPipesModule } from './02-deprecated-pipes/deprecated-pipes.module';
 import { UsePipeInCodeModule } from './03-use-pipe-in-code/use-pipe-in-code.module';
 import { UsePurePipeModule } from './04-use-pure-pipe/use-pure-pipe.module';
 import { UsePipeResultModule } from './05-use-pipe-result/use-pipe-result.module';
@@ -15,7 +14,6 @@ import { UsePipeResultModule } from './05-use-pipe-result/use-pipe-result.module
     BrowserModule,
 
     BuiltInPipesModule,
-    DepricatedPipesModule,
     UsePipeInCodeModule,
     UsePurePipeModule,
     UsePipeResultModule
