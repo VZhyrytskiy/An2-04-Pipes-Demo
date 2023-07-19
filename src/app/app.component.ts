32276cf1aaec96e6e53e506e9025ffc66b5fc144
VZhyrytskiy <Vitaliy_Zhyrytskyy@epam.com>
2023-07-19 16:47:36 +0300

    ref: transform to standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { AsyncObservablePipeComponent, CurrencyPipeComponent, DatePipeComponent, I18nPluralPipeComponent, I18nSelectPipeComponent, JsonPipeComponent, KeyValuePipeComponent, LowerUpperCasePipeComponent, NumberPipeComponent, PercentPipeComponent, SlicePipeComponent } from './01-built-in-pipes';
+import { ListComponent } from './03-use-pure-pipe/list.component';
+import { UsePipeComponent } from './02-use-pipe-in-code/use-pipe.component';
+import { UsePipeResultComponent } from './04-use-pipe-result/use-pipe-result.component';
+
+const components = [
+  AsyncObservablePipeComponent,
+  CurrencyPipeComponent,
+  DatePipeComponent,
+  I18nPluralPipeComponent,
+  I18nSelectPipeComponent,
+  JsonPipeComponent,
+  KeyValuePipeComponent,
+  LowerUpperCasePipeComponent,
+  NumberPipeComponent,
+  PercentPipeComponent,
+  SlicePipeComponent,
+  UsePipeComponent,
+  ListComponent,
+  UsePipeResultComponent
+];
+
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
+  imports: [...components]
 })
 export class AppComponent {
   title = 'Pipes Demo!';
---
+++ src/app/app.config.ts
@@ -0,0 +1,8 @@
+import { DEFAULT_CURRENCY_CODE } from "@angular/core";
+
+export const config = {
+  providers: [{ provide: DEFAULT_CURRENCY_CODE, useValue: 'GBP' }]
+};
+
+
+
