dee25051b2a93a498ac6da6c019bca521545313a
VZhyrytskiy <Vitaliy_Zhyrytskyy@epam.com>
2023-07-21 10:30:00 +0300

    ref: add ApplicationConfig type

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
-import { DEFAULT_CURRENCY_CODE } from "@angular/core";
+import { ApplicationConfig, DEFAULT_CURRENCY_CODE } from "@angular/core";
 
-export const config = {
+export const config: ApplicationConfig = {
   providers: [{ provide: DEFAULT_CURRENCY_CODE, useValue: 'GBP' }]
 };
 
